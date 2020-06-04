0efcecef5a4bfedbdf340a77cfa08d50fdead146
Joshua Chinemezu <joshuachinemezu@gmail.com>
2020-06-04 20:38:27 +0400

    Add helper files

---
+++ lib/src/elements/circular_loader.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CircularLoader extends StatefulWidget {
+  double height;
+
+  CircularLoader({Key key, this.height}) : super(key: key);
+
+  @override
+  _CircularLoaderState createState() => _CircularLoaderState();
+}
+
+class _CircularLoaderState extends State<CircularLoader> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController animationController;
+
+  void initState() {
+    super.initState();
+    animationController = AnimationController(duration: Duration(milliseconds: 300), vsync: this);
+    CurvedAnimation curve = CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+    animation = Tween<double>(begin: widget.height, end: 0).animate(curve)
+      ..addListener(() {
+        if (mounted) {
+          setState(() {});
+        }
+      });
+    Timer(Duration(seconds: 10), () {
+      if (mounted) {
+        animationController.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+//    Timer(Duration(seconds: 30), () {
+//      //if (mounted) {
+//      //}
+//    });
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: animation.value / 100 > 1.0 ? 1.0 : animation.value / 100,
+      child: SizedBox(
+        height: animation.value,
+        child: new Center(
+          child: new CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
