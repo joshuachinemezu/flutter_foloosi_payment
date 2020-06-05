589af0f00a50900683a69cf48239eb93a265f2d0
Joshua Chinemezu <joshuachinemezu@gmail.com>
2020-06-05 15:06:40 +0400

    Export package

--- lib/foloosi_payment.dart
+++ lib/foloosi_payment.dart
@@ -1,7 +1,1 @@
-library foloosi_payment;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:foloosi_payment/src/foloosi_payment.dart';
